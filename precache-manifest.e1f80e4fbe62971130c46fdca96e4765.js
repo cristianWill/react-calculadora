c272860aba4fe37ba55eb13e9c37507d2892aeb0
Cristian William <Cristianwlm@gmail.com>
2020-05-27 15:09:11 -0300

    Updates

---
+++ precache-manifest.e1f80e4fbe62971130c46fdca96e4765.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ef80c1f3b6d731f1efd2aaffde0eab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "268dcd6a0f71548c738e",
+    "url": "/static/css/main.ae531eec.chunk.css"
+  },
+  {
+    "revision": "da10f8f735a0c7b440d7",
+    "url": "/static/js/2.721727c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.721727c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268dcd6a0f71548c738e",
+    "url": "/static/js/main.36a0be29.chunk.js"
+  },
+  {
+    "revision": "17c2f7966a268bc0a23f",
+    "url": "/static/js/runtime-main.2af80ef2.js"
+  },
+  {
+    "revision": "288302ea531af8be59f6ac2b5bbbfdd3",
+    "url": "/static/media/RobotoMono-Thin.288302ea.ttf"
+  }
+]);
