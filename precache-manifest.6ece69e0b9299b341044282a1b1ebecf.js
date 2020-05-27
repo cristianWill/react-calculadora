a00f2eaa6d7626a90837c2ee608c2d1423efa4c5
Cristian William <Cristianwlm@gmail.com>
2020-05-27 15:19:54 -0300

    Updates

---
+++ precache-manifest.6ece69e0b9299b341044282a1b1ebecf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976f13369a5a26c771efb4cb359883ca",
+    "url": "/react-calculadora/index.html"
+  },
+  {
+    "revision": "0c3fbdeccada7d11dd63",
+    "url": "/react-calculadora/static/css/main.e0a525b4.chunk.css"
+  },
+  {
+    "revision": "da10f8f735a0c7b440d7",
+    "url": "/react-calculadora/static/js/2.721727c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculadora/static/js/2.721727c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c3fbdeccada7d11dd63",
+    "url": "/react-calculadora/static/js/main.a19e3d5a.chunk.js"
+  },
+  {
+    "revision": "9bab8c6e5fc5b81cd48f",
+    "url": "/react-calculadora/static/js/runtime-main.20ae80e8.js"
+  },
+  {
+    "revision": "288302ea531af8be59f6ac2b5bbbfdd3",
+    "url": "/react-calculadora/static/media/RobotoMono-Thin.288302ea.ttf"
+  }
+]);
